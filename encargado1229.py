20a1f36a69b2bf06af70330edfe694f0bc6330fd
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-09-26 11:55:51 -0600

    Add files via upload
    examen 1229

---
+++ encargado1229.py
@@ -0,0 +1,59 @@
+print("DIEGO ALI LEDEZMA CARBAJAL 1229")
+class encargado1229:
+    nombre1229=""
+    id_encargado1229=0
+    años1229=0
+    rfc1229=0
+    nacionalidad1229=""
+    estudios1229=""
+
+    def mostrar_datos(self):
+        print("ali")
+    def listar_encargados(self):
+            print("--LISTA ENCARGADOS--")
+     
+            lista1229 = ["1: JORGE", "2: MARIA", "3: RAUL", "4: ALEX", "5: JOSE", "6: ALI", "4: DIEGO"]
+            print(lista1229)
+    def tupla_perfumes1229(self):
+                print("--TUPLAS PERFUMES--")
+                tupla1229 = ("JPG", "YSL", "DIOR", "PARFUM DE MARLEY", "COACH", "DOLCE GABBANA", "PACCO RABBANE")
+                print(tupla1229)
+    def dicc_precio1229(self):
+                print("--DICCIONARIO PRECIOS PERFUMES--")
+                dicc1229 = {
+                "JPG $": 2500,
+                "YSL $": 3000,
+                "DIOR $": 4500,
+                "PARFUM DE MARLEY $": 7000,
+                "COACH $": 1600,
+                "DOLCE GABBANA $": 3400,
+                "PACO RABBANE $": 1900
+                }
+                for x in dicc1229:
+                    print(dicc1229[x])
+                
+    def altas1229(self):
+                print("la operacion se realizo correctamente para los datos del encargado, 1229")
+    def bajas1229(self):
+                print("la operacion se realizo correctamente para los datos del encargado, 1229")
+        
+perfumeali1229=encargado1229()
+
+print("funcion lista")
+perfumeali1229.mostrar_datos()
+perfumeali1229.listar_encargados()
+perfumeali1229.tupla_perfumes1229()
+perfumeali1229.dicc_precio1229()
+perfumeali1229.altas1229()
+perfumeali1229.bajas1229()
+
+
+
+
+
+
+
+
+
+
+
